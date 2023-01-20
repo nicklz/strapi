88fe4053214cb59364c39ab1366ee08f6ed4de1b
Nick Kuhn <nicklz22@yahoo.com>
2023-01-20 02:41:55 -0800

    first working version

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f8483318fd094b026c7dd62ad842986b'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9198cb56-0587-45cd-b83c-a6b5d3150ce8'
+};
